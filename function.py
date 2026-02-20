7287bc3e3837fee394899c75a3ebc055fcd2ce5f
apeksha-ka <abhishekgowda@Abhisheks-MacBook-Pro-2.local>
2026-02-20 11:42:08 +0530

    Added function

---
+++ function.py
@@ -0,0 +1,19 @@
+
+def even_odd(num):
+    if num % 2 == 0:
+        print(num, "is Even number")
+    else:
+        print(num, "is Odd number")
+
+
+number = int(input("Enter a number: "))
+
+
+even_odd(number)
+
+
+        
+    
+
+    
+    
