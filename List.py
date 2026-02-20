7cbe5369d81d6ea5362930cb7d6e2eee756953b0
apeksha-ka <abhishekgowda@Abhisheks-MacBook-Pro-2.local>
2026-02-20 14:35:10 +0530

    Added list

---
+++ List.py
@@ -0,0 +1,9 @@
+
+def function_1(data):
+    ret_list = []
+    for val in data:
+        if val%2 != 0:
+            ret_list.append(val)
+
+    return ret_list
+print(function_1([2,3,4,5,6,7,8,9,11,12,14,18]))
